d22c7bbb5fb3f9509960effbdb4faff543bcf1df
MojoMiojo <mjasousa@sga.pucminas.br>
2022-06-01 19:35:22 -0300

    Criando o projeto iOSDogAge

---
+++ IOSDogAge/IOSDogAge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSDogAge
+//
+//  Created by Mozart Sousa on 01/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
