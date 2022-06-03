c7f8e63c9af2bb3e116c6d051dd559ad4573fedf
Mozart Sousa <mjasousa@sga.pucminas.br>
2022-06-02 23:41:09 -0300

    Feat interação com o button e label (#5)
    * Implementando a interface
    
    * Interacao com Button e Label

--- IOSDogAge/IOSDogAge/ViewController.swift
+++ IOSDogAge/IOSDogAge/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func discoverAge(_ sender: Any) {
+        resultLabel.text = "Resultado alterado!"
+    }
 
 }
 
